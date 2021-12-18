75ed3d00e120d51f421be5482acfa7e569979877
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2021-12-18 14:13:43 -0800

    added intro component to app.js

--- hlp-blog/src/App.js
+++ hlp-blog/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Intro from './components/Intro'
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path='/' element={<Intro />} />
+      </Routes>
     </div>
   );
 }
