dfd56e74364c92bc7f2ebb463b7c94de55348bfd
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2021-12-19 11:48:39 -0800

    updated BlogList component, created .env file

--- hlp-blog/src/components/BlogList.js
+++ hlp-blog/src/components/BlogList.js
@@ -1,19 +1,50 @@
 // import { Link } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'; 
 
-function BlogList() {
+const BlogList = () => {
+    const [blogList, setBlogList] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // console.log(process.env.REACT_APP_API_URL)
+                const res = await axios.get(`http://localhost:8000/blog_posts/`)
+                // useState to set a list
+                setBlogList(res.data)
+                
+            }
+            catch (err) {
+
+            }
+        }
+        fetchData();
+    }, [])
+
+    let listHTML = blogList.map((listItem) => {
+        return (
+            <li>{listItem.title}</li>
+        )
+    })
+
     return(
         <div>
             <h3>Entries</h3>
-            {/* I need to make an API call to my backend */}
-            {/* It must return a list of all blog entries */}
-            {/* The list must show the title and the date of creation */}
+            {listHTML}
+            {/* I need to set a state for the list of blog entries */}
+            {/* I need to make an API call to my backend  which sets this state */}
+            {/* the state is an array of objects */}
+            {/* in the return, we return something like {blogList.title} and {bloglist.created_at} */}
             {/* The title needs to link to a Route which shows the content of the post */}
 
-            {/* First goal: get route working */}
-            {/* Second goal: have api call return anything from backend */}
-            {/* Third goal: api call returns title and created date from backend for all entries */}
+            
         </div>
     )
 };
 
 export default BlogList;
+
+// Second goal: have api call return anything from backend
+//     ? useEffect, axios, async/away ?
+// Third goal: api call returns title and created date from backend for all entries
+
