d637a28602ba81760ddd9483e9d4421dd65519da
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-01-22 14:17:31 -0800

    added link to contact page to blog posts

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,13 @@
+import Contact from "./Contact"
+import { Link } from "react-router-dom"
 
 const Blog = (props) => {
+
     function clearState() {
         props.setTitle('')
         props.setContent('')
     }
+    console.log(props.title)
     
     // This function takes the content state and adds react's '___html' key
     const blogContent = () => {
@@ -17,6 +21,7 @@ const Blog = (props) => {
         <div className='blog-post'>
             <h3>{props.title}</h3>
             <p className='date'>Last updated: <span className='date-slice'>{dateSlice}</span></p>
+            <p className='contact'>If you would like to contact the author of this site, please click <Link to='/contact'>here</Link>.</p>
             <div className='content' dangerouslySetInnerHTML={blogContent()} />
             <button onClick={clearState}>Homepage</button>
         </div>
